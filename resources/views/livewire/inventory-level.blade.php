14d70d5fb10a1fa2075e67ac43363c37d73f4c9a
michaelneumaier <mikeneumaier@gmail.com>
2024-11-03 23:31:02 -0500

    another solution for accidental double tap zooming

--- resources/views/livewire/inventory-level.blade.php
+++ resources/views/livewire/inventory-level.blade.php
@@ -1,6 +1,6 @@
 <div>
     @if($listItem && isset($currentValue))
-    <div class="flex items-center space-x-0.5">
+    <div class="flex items-center space-x-0.5 touch-manipulation">
         @php
         $isHalf = ($currentValue - intval($currentValue)) >= 0.5;
         @endphp
